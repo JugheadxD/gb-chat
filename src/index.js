45064069b75fde1ed0020bcb2b5c8bcb3ef79639
Александр Кобалия <jughead@MacBook-Air-Aleksandr.local>
2022-03-24 13:21:03 +0300

    lesson_2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
+import "./index.css";
 
-const App = (props) => {
-  return props.title;
-};
+const MessageList = () => {
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState([
+    {
+      author: "bot",
+      text: "Hello from Bot",
+      date: new Date().toLocaleTimeString(),
+    },
+  ]);
 
-const Message = () => {
+  useEffect(() => {
+    const lastMessage = message[message.length - 1];
+    let timerId = null;
+
+    if (message && lastMessage.author === "user") {
+      timerId = setTimeout(() => {
+        setMessage([...message, { author: "bot", text: "Hello from Bot" }]);
+      }, 1500);
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [message]);
+
   return (
-    <div className='testDiv'>
-      <App title='test1' />
+    <div>
+      <h2>
+        <input
+          className="input"
+          placeholder="Введите текст..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button
+          className="shine-button"
+          onClick={() =>
+            setMessage([...message, { author: "user", text: value }])
+          }
+        >
+          Отправить
+        </button>
+        <hr />
+        {message.map((m) => (
+          <div>
+            <h1>{m.author}</h1>
+            <p>{m.text}</p>
+            <p>{m.date}</p>
+            <hr />
+          </div>
+        ))}
+      </h2>
     </div>
   );
 };
 
+const App = () => {
+  return <MessageList />;
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Message />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
